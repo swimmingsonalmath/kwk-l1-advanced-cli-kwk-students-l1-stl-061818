92b7b73e0eb8e88ec3c737ec0f68567b9485d010
Sonali Sharma <sonalisharma58521@gmail.com>
2018-07-01 20:14:54 +0000

    Done.

---
+++ test_script.sh
@@ -0,0 +1,10 @@
+ls;
+mkdir
+this_is_a_directory_made_in_a_script ;
+mkdir another_directory;
+touch
+this_is_a_directory_made_in_a_script/first_file.txt;
+touch
+this_is_a_directory_made_in_a_script/second_file.txt;
+mv
+this_is_a_directory_made_in_a_script/second_file.txtanother_directory
